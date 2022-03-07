ce268f7874b75b5f054783ca231b44279b1c11d5
Mewtwoshaurd <mewtwoshard@gmail.com>
2022-03-07 08:25:26 +0000

    updated main.cpp with comments to remove reference to port not accessible on dev board c

--- ultraviolet-taproot/src/main.cpp
+++ ultraviolet-taproot/src/main.cpp
@@ -83,7 +83,7 @@ int main()
 
         if (sendMotorTimeout.execute())
         {
-            PROFILE(drivers->profiler, drivers->mpu6500.periodicIMUUpdate, ());
+            //PROFILE(drivers->profiler, drivers->mpu6500.periodicIMUUpdate, ());
             PROFILE(drivers->profiler, drivers->commandScheduler.run, ());
             PROFILE(drivers->profiler, drivers->djiMotorTxHandler.processCanSendData, ());
             PROFILE(drivers->profiler, drivers->terminalSerial.update, ());
@@ -102,7 +102,7 @@ static void initializeIo(src::Drivers *drivers)
     drivers->can.initialize();
     drivers->errorController.init();
     drivers->remote.initialize();
-    drivers->mpu6500.init();
+    //drivers->mpu6500.init();
     drivers->refSerial.initialize();
     drivers->terminalSerial.initialize();
     drivers->schedulerTerminalHandler.init();
@@ -118,5 +118,5 @@ static void updateIo(src::Drivers *drivers)
     drivers->canRxHandler.pollCanData();
     drivers->refSerial.updateSerial();
     drivers->remote.read();
-    drivers->mpu6500.read();
+    //drivers->mpu6500.read();
 }
